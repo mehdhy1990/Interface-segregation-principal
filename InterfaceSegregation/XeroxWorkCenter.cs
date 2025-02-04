5e00c348c72bcae166f23d03dde048b71d341130
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-04 15:05:45 +0300

    class made to use Multi purpose Interface

---
+++ InterfaceSegregation/XeroxWorkCenter.cs
@@ -0,0 +1,34 @@
+﻿namespace InterfaceSegregation;
+
+public class XeroxWorkCenter : IMultiFunction
+{
+    public void Print()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void GetPrintDetail()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Scan()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void ScanPhoto()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Fax()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void InternetFax()
+    {
+        throw new NotImplementedException();
+    }
+}
