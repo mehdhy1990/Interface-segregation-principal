ced82d3a02d6f8e62df354199492fd2f701304dc
Mehdi Heidari <hymehdi84@gmail.com>
2025-02-04 15:04:09 +0300

    multi task interface created

---
+++ InterfaceSegregation/IMultiFunction.cs
@@ -0,0 +1,11 @@
+﻿namespace InterfaceSegregation;
+
+public interface IMultiFunction
+{
+    public void Print();
+    public void GetPrintDetail();
+    public void Scan();
+    public void ScanPhoto();
+    public void Fax();
+    public void InternetFax();
+}
